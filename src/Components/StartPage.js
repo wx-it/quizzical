e9fa01bfed54b51e8c654cf03f5f9f2bafa4308a
wx-it <zinebbendjafer9@gmail.com>
2022-09-19 18:44:32 +0100

    start page done

---
+++ src/Components/StartPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const StartPage = () => {
+  return (
+    <div className="container">
+        <h2>Quizzical</h2>
+        <p>Test your knowledge on various topics</p>
+        <button>Start quiz</button>
+    </div>
+  )
+}
+
+export default StartPage
