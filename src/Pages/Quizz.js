784d406c6c72aa6baedd46b7e990f3b92fdb071d
wx-it <zinebbendjafer9@gmail.com>
2022-10-02 19:51:50 +0100

    fetch questions

--- src/Pages/Quizz.js
+++ src/Pages/Quizz.js
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import { useEffect } from 'react'
 const Quizz = ({data}) => {
     console.log(data)
+    useEffect(()=>{
+        dataQuestions()
+    },[])
+
+    const dataQuestions = async() =>{
+        const fetchData = await fetch('https://opentdb.com/api.php?amount=10&difficulty=easy')
+        const response = await fetchData.json()
+        const data = console.log(response)
+      }
+    
+
   return (
     <div>
-        
+
     </div>
   )
 }
