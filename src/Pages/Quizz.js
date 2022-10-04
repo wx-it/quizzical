e79dff5b732dd1abd4ef79d16146e46280ea7480
wx-it <zinebbendjafer9@gmail.com>
2022-10-04 18:53:13 +0100

    matching preferences

--- src/Pages/Quizz.js
+++ src/Pages/Quizz.js
@@ -1,21 +1,28 @@
 import React from 'react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 const Quizz = ({data}) => {
-    console.log(data)
     useEffect(()=>{
         dataQuestions()
     },[])
 
     const dataQuestions = async() =>{
-        const fetchData = await fetch('https://opentdb.com/api.php?amount=10&difficulty=easy')
+        const fetchData = await fetch(`https://opentdb.com/api.php?amount=10&category=${data.category}&difficulty=${data.difficulty}&type=multiple`)
         const response = await fetchData.json()
-        const data = console.log(response)
+        const questionsData = response.results
+        setQuestions(prevQuestions => questionsData)
       }
     
 
+      const [questions, setQuestions] = useState([])
+
   return (
     <div>
-
+      {questions.map((questions)=>(
+          <div>
+            <p>{questions.question}</p>
+          </div>
+        )
+      )}
     </div>
   )
 }
