b3abe6e41c4398254bb452c80203cd69b5874cfb
wx-it <96913718+wx-it@users.noreply.github.com>
2022-10-19 18:41:29 +0100

    installed nanoid

--- src/Components/Questions.jsx
+++ src/Components/Questions.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
+import {nanoid} from 'nanoid'
+import {useState} from 'react'
 
 const Questions = ({questions}) => {
+
+  const[color, setColor] = useState(false)
+
+  function changeColor(){
+    setColor(prevColor=> !prevColor)
+  }
+
+  const Style = {
+    backgroundColor: 'black'
+  }
+
   return (
     <>
           {questions.map((questions)=>(
-          <div className='quiz-questions'>
-            <p className='question' key={} >{questions.question}</p>
-           <div className="answers">
-              <p className='correct-answer answer'>{questions.correct_answer}</p>
-            <div className='incorrect-answers'>{questions.incorrect_answers.map((answer)=>{
+          <div className='quiz-questions' key={nanoid.id}>
+            <p className='question' key={nanoid.id}>{questions.question}</p>
+           <div className="answers" key={nanoid.id}>
+              <p className={color ? {Style} : 'answer'} key={nanoid.id} onClick={changeColor}>{questions.correct_answer}</p>
+            <div className='incorrect-answers' key={nanoid.id}>{questions.incorrect_answers.map((answer)=>{
               return(
-                <p className='answer incorrect-answer'>{answer}</p>
+                <p className='answer incorrect-answer' key={nanoid.id}>{answer}</p>
               )
             })}</div>
            </div>
