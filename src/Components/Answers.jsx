9eda5ed0f60bcbcecd6078ba030bfa1285238594
wx-it <96913718+wx-it@users.noreply.github.com>
2022-10-31 19:45:44 +0100

    filtered out duplicates + randomize the order

--- src/Components/Answers.jsx
+++ src/Components/Answers.jsx
@@ -7,12 +7,12 @@ const Answers = ({questions}) => {
         let incorrect = questions.incorrect_answers
         let correct = questions.correct_answer
         incorrect.push(correct)
-        return incorrect.map((answer)=>(
+        let allAnswers = [...new Set(incorrect)] 
+        allAnswers.sort(function(a, b){return 0.5 - Math.random()})
+        return allAnswers.map((answer)=>(
             <p className='answer'>{answer}</p>
         ))
     }
-
-    //filter out all the same answers
 
   return (
     <>
